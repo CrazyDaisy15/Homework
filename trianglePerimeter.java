0c6c038e14af8b5e408c449b2cce67e12c672baf
CrazyDaisy <musakova87@gmail.com>
2024-01-28 20:14:12 +0200

    Task Solved

---
+++ trianglePerimeter.java
@@ -0,0 +1,8 @@
+public class trianglePerimeter {
+    public static void main(String[] args) {
+        double a = 5;
+        double b = 7;
+        double c = 3;
+        System.out.println("P="+ (a + b + c));
+    }
+}
