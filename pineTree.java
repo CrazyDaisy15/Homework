2a0caa991a878fdefb2bd13cf32a4f527d38840e
CrazyDaisy <musakova87@gmail.com>
2024-01-28 20:13:52 +0200

    Task Solved

---
+++ pineTree.java
@@ -0,0 +1,11 @@
+public class pineTree {
+    public static void main(String[]args) {
+     System.out.println("     *     ");
+     System.out.println("    ***    ");
+     System.out.println("   *****   ");
+     System.out.println("  *******  ");
+     System.out.println(" ********* ");
+     System.out.println("***********");
+     System.out.println("    | |    ");
+    }
+}
