7210230ed21d04022f73c88d8d0a96c60d31f9de
CrazyDaisy <musakova87@gmail.com>
2024-01-28 18:14:45 +0200

    First Task Solved

---
+++ wholeName.java
@@ -0,0 +1,9 @@
+public class wholeName {
+    public static void main(String[] args) {
+    String firstName = "Margarita";
+    String middleName = "Georgieva";
+    String lastName = "Musakova";
+    String fullName = firstName + " " + middleName + " " + lastName;
+    System.out.println("Full Name:"+fullName);
+}
+}
