8aff11c3cbaf3fb45467e6be27744106bf34fb29
CrazyDaisy <musakova87@gmail.com>
2024-02-04 18:23:02 +0200

    Task Solved

--- salesRevenue.java
+++ salesRevenue.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class salesRevenue {
     public static void main(String[] Strings) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Units sold = ");
+        System.out.print("Еnter unit price: ");
+        float unitPrice = Float.parseFloat(scan.nextLine());
+        System.out.print("Enter quantity: ");
         int unitsSold = Integer.parseInt(scan.nextLine());
-        System.out.print("Sales price = ");
-        float salesPrice = Float.parseFloat(scan.nextLine());
         double salesRevenue;
         double discountRate = 0;
 
-        salesRevenue = unitsSold * salesPrice;
+        salesRevenue = unitsSold * unitPrice;
 
         if (unitsSold >= 100 && unitsSold <= 120) {
             discountRate = 0.15;
@@ -20,6 +20,6 @@ public class salesRevenue {
         double discountedRevenue = salesRevenue - discountAmount;
 
         System.out.printf("The revenue from sale: $%.2f\n", discountedRevenue);
-        System.out.printf("The discount rate you received is %.0f%%.\n", discountRate * 100);
+        System.out.printf("Discount: %.0f%%.\n", discountRate * 100);
     }
 }
