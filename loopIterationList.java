41d822ea739a7765f1a167e399fce80f55d9392a
CrazyDaisy <musakova87@gmail.com>
2024-02-04 20:58:43 +0200

    Task Solved

---
+++ loopIterationList.java
@@ -0,0 +1,14 @@
+public class loopIterationList {
+    public static void main(String[] args) {
+        int[] numbers = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+        System.out.println("Numbers divisible by 5: ");
+        for (int number : numbers) {
+            if (number > 150) {
+                break;
+            }
+            if (number % 5 == 0) {
+                System.out.print(number + ", ");
+            }
+        }
+    }
+}
